b0e26df3d98afc30b5cda7454ced47f21880738f
Ryan Dwyer <ryanpdwyer@gmail.com>
2014-07-28 08:31:25 -0400

    Updated the blog pelican stuff, added an example notebook.

---
+++ content/post.md
@@ -0,0 +1,9 @@
+Title: My First Blog Post
+Date: 2013-10-14
+Category: Thoughts
+Tags: blogging, python, whateveryouwant
+Slug: my-first-post
+Author: Me
+Summary: Where I detail my first post
+
+Hello world, here's my first post!
--- pelicanconf.py
+++ pelicanconf.py
@@ -4,13 +4,43 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Ryan Dwyer'
 SITENAME = u"Ryan's blog"
-SITEURL = 'http://ryanpdwyer.github.io'
+SITEURL = ''
 
 PATH = 'content'
 
-TIMEZONE = ''
+TIMEZONE = 'America/New_York'
 
 DEFAULT_LANG = u'en'
+
+OUTPUT_RETENSION = (".hg", ".git", ".bzr")
+
+THEME = "pelican-elegant"
+
+PLUGIN_PATHS = ["pelican-plugins"]
+
+PLUGINS = ['sitemap', 'extract_toc', 'tipue_search',
+           'neighbors', 'render_math']
+
+MD_EXTENSIONS = ['codehilite(css_class=highlight)', 'extra', 'headerid', 'toc']
+DIRECT_TEMPLATES = (('index', 'tags', 'categories', 'archives',
+                    'search', '404'))
+
+STATIC_PATHS = ['theme/images', 'images', 'files', 'notebooks',
+                'pelican-plugins']
+
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly'
+    }
+}
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
